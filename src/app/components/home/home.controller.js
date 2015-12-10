5d2ee660614900bcecfd85e8b9cb8a8a35e1de0a
Jorge Rivera <jorger@vsisoft.com>
2015-12-10 00:56:53 -0600

    home.template.html modified

--- src/app/components/home/home.controller.js
+++ src/app/components/home/home.controller.js
@@ -2,7 +2,7 @@
   'use strict';
 
   angular.module('personal-site')
-    .controller('homeCtrlr', [function(){
-      
+    .controller('homeCtrlr', ['$scope', function($scope){
+      $scope.pagina = 'home';
     }]);
 }());
--- src/app/routes.js
+++ src/app/routes.js
@@ -2,18 +2,24 @@
   'use strict';
 
   angular.module('personal-site')
-    .config(['$routeProvider', function($routeProvider){
-      $routeProvider
-        .when('/home', {
-          templateUrl: 'app/components/home/home.template.html',
-          controller: 'homeCtrlr'
-        })
-        .when('/blog', {
-          templateUrl: 'app/components/blog/blog.template.html',
-          controller: 'blogCtrlr'
-        })
-        .otherwise({
-          redirectTo: '/home'
-        });
+    .config([
+      '$routeProvider',
+      '$locationProvider',
+      function($routeProvider, $locationProvider){
+        $routeProvider
+          .when('/home', {
+            templateUrl: 'app/components/home/home.template.html',
+            controller: 'homeCtrlr'
+          })
+          .when('/blog', {
+            templateUrl: 'app/components/blog/blog.template.html',
+            controller: 'blogCtrlr'
+          })
+          .otherwise({
+            redirectTo: '/home'
+          });
+
+        $locationProvider
+          .html5Mode(true);
     }]);
 }());

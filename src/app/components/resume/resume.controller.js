0b8417cdf6ff1901bd17c06a22960e8d718cdd19
Jorge Rivera <jorgeriv@gmail.com>
2016-05-16 21:50:03 -0500

    load programming languages in resume from controller

--- src/app/components/resume/resume.controller.js
+++ src/app/components/resume/resume.controller.js
@@ -19,5 +19,13 @@
         {name: 'REST'},
         {name: 'Gulp'}
       ];
+
+      $scope.programmingLanguages = [
+        {name: 'JavaScript', proficency:95, level:'Expert'},
+        {name: 'HTML5', proficency:90, level:'Advanced'},
+        {name: 'CSS3', proficency:88, level:'Advanced'},
+        {name: 'Python', proficency:25, level:'Beginner'},
+        {name: 'SQL', proficency:18, level:'Beginner'},
+      ];
     }
 }());

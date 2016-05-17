3dd073eca0452e96f2c845360962f70eae2ad9c1
Jorge Rivera <jorgeriv@gmail.com>
2016-05-16 21:43:38 -0500

    load resume skillset from resume controller

--- src/app/components/resume/resume.controller.js
+++ src/app/components/resume/resume.controller.js
@@ -2,6 +2,22 @@
   angular.module('resume')
     .controller('resumeController',['$scope', resumeController]);
     function resumeController($scope){
+      $scope.librariesAndFrameworks = [
+        {name: 'jQuery'},
+        {name: 'Express.js'},
+        {name: 'Angular.js'},
+        {name: 'Backbone.js'},
+        {name: 'Bootstrap'},
+        {name: 'Purecss'},
+        {name: 'Sass'},
+        {name: 'Require.js'}
+      ];
 
+      $scope.tools = [
+        {name: 'GIT'},
+        {name: 'SVN'},
+        {name: 'REST'},
+        {name: 'Gulp'}
+      ];
     }
 }());
